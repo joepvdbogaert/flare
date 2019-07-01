a3703a08f7617e3d3805ebf3bda499e346a99091
Bogaert <j.v.d.bogaert@tue.nl>
2019-07-01 16:21:12 +0200

    Add train-test split by year and adjust evaluation for multi-class cases

--- flare/evaluation.py
+++ flare/evaluation.py
@@ -1,7 +1,33 @@
 import numpy as np
 import pandas as pd
 
+import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error
+
+
+def train_test_split_by_year(data, years_test=2, year_col="YEAR"):
+    """Split a dataset in train and test by year.
+
+    Parameters
+    ----------
+    data: pd.DataFrame
+        The data to split.
+    years_test: int, default=2
+        The number of years to reserve for testing.
+    year_col: str, default="YEAR"
+        The column with the year.
+
+    Returns
+    -------
+    train, test: pd.DataFrame
+        The train and test sets respectively.
+    """
+    years = np.sort(np.unique(data[year_col]))
+    cut = years[-years_test]
+    print("Train contains [{}, {}], test set [{}, {}]."
+          .format(years[0], cut - 1, cut, years[-1]))
+
+    return data[data[year_col] < cut], data[data[year_col] >= cut]
 
 
 class YearSplitter():
@@ -59,13 +85,18 @@ def evaluate_model(model, x, y, scoring, score_on_proba=False):
         Used to calculate the score. Must take true_label, predicted_label as inputs.
     """
     if score_on_proba:
-        y_hat = model.predict_proba(x)
+        y_hat = np.array(model.predict_proba(x))
+        if len(y_hat.shape) == 3:
+            # received list of predictions, keep proba of 1 for each class
+            y_hat = y_hat[:, :, 1]
+            y_hat = y_hat.transpose()
         if y_hat.shape[1] == 2:
             # binary prediction, keep proba of 1
             y_hat = y_hat[:, 1].flatten()
     else:
-        y_hat = model.predict(x)
-    score = scoring(y, np.asarray(y_hat, dtype=np.float))
+        y_hat = np.array(model.predict(x))
+
+    score = scoring(y, y_hat)
     return score
 
 def cross_validate_by_year(model_cls, data, x_cols, y_col, model_params=None, folds=4,
@@ -121,3 +152,50 @@ def cross_validate_by_year(model_cls, data, x_cols, y_col, model_params=None, fo
         return val_scores
     else:
         return np.mean(val_scores)
+
+
+# function copied from sklearn examples
+def plot_confusion_matrix(y_true, y_pred, classes, normalize=False, title=None,
+                          cmap=plt.cm.Blues):
+    """This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix'
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    classes = classes[unique_labels(y_true, y_pred)]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return fig
